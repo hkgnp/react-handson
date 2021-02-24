df40689807800e814373ff372c9b3a8aa600a58c
hkgnp <benpng@u.nus.edu>
2021-02-24 23:43:25 +0800

    Completed Q10

--- 09-list-rendering/src/GuestList.js
+++ 09-list-rendering/src/GuestList.js
@@ -1,32 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 class GuestList extends React.Component {
-    state = {
-        'guests': [
-            'Tony Stark',
-            'Hawkeye',
-            'Spider Man',
-            'Bruce Wayne'
-        ]
-    }
+  state = {
+    guests: ['Tony Stark', 'Hawkeye', 'Spider Man', 'Bruce Wayne'],
+  };
 
-    render() {
-        return (
-            <React.Fragment>
-                <ul>
-                    {this.renderGuestList()}
-                </ul>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <ul>{this.renderGuestList()}</ul>
+      </React.Fragment>
+    );
+  }
 
-    renderGuestList = () => {
-        let g = [];
-        for (let guest of this.state.guests) {
-            g.push(<li>{guest}</li>)
-        }
-        return g;
+  renderGuestList = () => {
+    let g = [];
+    for (let guest of this.state.guests) {
+      g.push(<li>{guest}</li>);
     }
+    return g;
+  };
 }
 
-export default GuestList
+export default GuestList;
